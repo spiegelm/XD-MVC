6e8113615fac3a119e7e14d8b1e2b905c116eb5f
Michael Spiegel <spiagl.m@gmail.com>
2016-02-12 19:59:52 +0100

    Revert Maps port to 8080

--- Examples/Maps/server.js
+++ Examples/Maps/server.js
@@ -8,6 +8,6 @@ var app = connect().use(serveStatic(__dirname + '/public'));
 var server = http.createServer(app);
 
 xdmvc.start(9010, 3010, 9011);
-var port = 8000;
+var port = 8080;
 server.listen(port);
 console.log("Run maps on port " + port + ". path /maps.html");
